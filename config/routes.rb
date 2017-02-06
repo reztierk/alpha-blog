9f7bb66f8538514e18eb3a47368aa3637ff04ffc
Michael Kreitzer <reztierk@gmail.com>
2017-02-06 18:49:11 +0000

    Completed authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   
   get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
   
