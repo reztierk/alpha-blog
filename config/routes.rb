05d9c3b307d1c5e4d3c1dcb9702f248e41764f4d
Michael Kreitzer <reztierk@gmail.com>
2017-02-07 00:26:25 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  resources :categories, except: [:destroy]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
